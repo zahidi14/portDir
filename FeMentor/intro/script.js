a827ee922c3b171dda8600b647cebba90c73209b
zahidi14 <d.zahidi1412@gmail.com>
2022-08-10 20:43:09 +0800

    teddsdt

--- FeMentor/intro/script.js
+++
@@ -1,9 +0,0 @@
-function response(){
-    var x = document.getElementById("resp");
-    if (x.className === ""){
-        x.classList.add("responsive");
-    }else{
-        x.classList.remove("responsive");
-    }
-    console.log("teste");
-}
